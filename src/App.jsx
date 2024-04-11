1bf3db881fd8db9713e1a6882b3380e32220ac46
bloodyrainday <bloodyrainday@gmail.com>
2024-04-11 12:57:49 +0300

    feat: add some style

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
 import Button from "./Button";
 import Icon from "./Icon";
 
+library.add(fas);
+
 function App() {
   const [randomIcon, setRandomIcon] = React.useState(0);
+
+  const arrFasIcons = Object.keys(library.definitions.fas);
+
+  console.log(randomIcon);
   return (
-    <div className="App">
-      <Icon randomIcon={randomIcon} />
-      <Button randomIcon={randomIcon} setRandomIcon={setRandomIcon} />
+    <div className="wrapper">
+      <Icon randomIcon={randomIcon} arrFasIcons={arrFasIcons} />
+      <Button setRandomIcon={setRandomIcon} arrFasIcons={arrFasIcons} />
     </div>
   );
 }
--- src/Icon.jsx
+++ src/Icon.jsx
@@ -2,37 +2,10 @@ import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-
-library.add(fas, fab, far);
-
-const iconStyles = ["fab", "fas", "far"];
-
-const chooseIconStyle = () => {
-  return [
-    iconStyles[Math.round(Math.random() * 3)],
-    arrFabIcons[Math.round(Math.random() * 3)],
-  ];
-};
-
-// for (let keys in library.definitions.fab) {
-//   console.log(keys);
-// }
-const arrFabIcons = Object.keys(library.definitions.fab);
-const arrFasIcons = Object.keys(library.definitions.fas);
-const arrFarIcons = Object.keys(library.definitions.far);
-console.log(arrFabIcons, arrFasIcons, arrFarIcons);
-
-const Icon = ({ randomIcon }) => {
+const Icon = ({ randomIcon, arrFasIcons }) => {
   return (
     <>
-      {}
-      <FontAwesomeIcon
-        icon={[`${iconStyles[0]}`, `${arrFabIcons[randomIcon]}`]}
-      />
+      <FontAwesomeIcon icon={["fas", `${arrFasIcons[randomIcon]}`]} />
     </>
   );
 };
